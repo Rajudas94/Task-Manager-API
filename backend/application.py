d9f87d4c55876998cb2f983d5b7166ddd69c2e74
Rajudas94 <rajud878@gmail.com>
2025-11-06 12:30:08 +0530

    Update DB config for PostgreSQL compatibility

--- backend/application.py
+++ backend/application.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'sqlite:///instance/tasks.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'sqlite:///instance/tasks.db').replace('postgres://', 'postgresql://')
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'super-secret')
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)
 
