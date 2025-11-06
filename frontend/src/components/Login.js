dbc75a1e1c5c4e8836f15353b18139db64af490a
Rajudas94 <rajud878@gmail.com>
2025-11-06 15:58:58 +0530

    Change of URL on the Login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -18,7 +18,7 @@ function Login(){
 
         try{
 
-           const response = await fetch("http://127.0.0.1:5000/login", {
+           const response = await fetch("https://task-manager-b4it.onrender.com/login", {
 
             method: "POST",
             headers: { "Content-Type" : "application/json" },
