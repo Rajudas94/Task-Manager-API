eb582166d56caf8493c2a37825d9d3fa6d8a6acb
Rajudas94 <rajud878@gmail.com>
2025-11-08 14:45:31 +0530

    Deleted .github directory and the test_basic.py

--- backend/test_basic.py
+++
@@ -1,7 +0,0 @@
-from application import app
-
-def test_home_route():
-    response = app.test_client().get('/')
-    assert response.status_code == 200
-    assert b"Task Manager API is running" in response.data
-
